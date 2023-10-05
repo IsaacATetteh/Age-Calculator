b2f8917466212b6dd94913cb6b13a48f080c93fe
IsaacATetteh <iabbeytetteh@gmail.com>
2023-10-05 02:47:13 +0100

    Updating Fonts

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div className="w-full h-full justify-center items-center flex bg-[#F0F0F0]">
       <div className="w-[600px] h-[500px] bg-[#ffffff] rounded-lg rounded-br-[175px]">
-        <div></div>
+        <div>
+          <p className="text-xl sans font">hello world</p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-poppins)"],
+      },
       colors: { "dusty-grey": "#7D7F7D" },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
