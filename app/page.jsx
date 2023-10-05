d138c5656e1a9dd177297d31967b619335329bd2
IsaacATetteh <iabbeytetteh@gmail.com>
2023-10-05 02:17:28 +0100

    Calculator layout

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full justify-center items-center flex bg-[#F0F0F0]">
+      <div className="w-[600px] h-[500px] bg-[#ffffff] rounded-lg rounded-br-[175px]">
+        <div></div>
+      </div>
+    </div>
+  );
+}
